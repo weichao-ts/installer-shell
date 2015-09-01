deebcfb7fe7449ef6ccfc61d9884fb19f23c11c4
tianshi <tbtianshi@gmail.com>
2015-09-01 14:04:23 +0800

    XX

--- redis/redis_init.sh
+++ redis/redis_init.sh
@@ -9,8 +9,8 @@ CLIEXEC=/usr/local/bin/redis-cli
 PIDFILE=/var/run/redis_${REDISPORT}.pid
 CONF="/etc/redis/${REDISPORT}.conf"
 if [ ! -f $CONF ]; then
-    wget http://download.redis.io/releases/hs_redis.conf
-    cp hs_redis.conf CONF
+    wget https://github.com/weichao-ts/installer-shell/blob/master/redis/hs_redis.conf
+    cp hs_redis.conf $CONF
 fi
 case "$1" in
     start)
